cf95f6ab833f20637cad995b3a8370eaec28d135
Danyl.S.John <167003177+Danyl101@users.noreply.github.com>
2025-08-01 16:26:22 +0530

    Delete Models directory

--- Models/bert.py
+++
@@ -1,7 +0,0 @@
-import torch
-import pandas as pd
-from transformers import BertTokenizer, BertModel
-import torch.nn as nn
-import numpy as np  
-
-data=pd.read_csv("data.csv") #Load the data from csv file
